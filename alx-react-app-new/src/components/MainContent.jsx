b34477c16a7f184d751ff1512ba4823672363385
Nick Odipo <nickodips@gmail.com>
2025-07-19 10:56:05 +0300

    Make changes to file MainContent

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#e6f2ff' }}>
-      <p>Welcome to the city explorer app! Browse through your favorite cities and learn fun facts.</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
