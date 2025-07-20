f5c09f7696606afce552ef6504af73ab4803b7e7
Nick Odipo <nickodips@gmail.com>
2025-07-20 18:35:20 +0300

    Add inline styling to Navbar component

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,12 +2,13 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white' }}>
-      <Link to="/" style={{ marginRight: '10px', color: 'white' }}>Home</Link>
-      <Link to="/about" style={{ marginRight: '10px', color: 'white' }}>About</Link>
-      <Link to="/services" style={{ marginRight: '10px', color: 'white' }}>Services</Link>
-      <Link to="/contact" style={{ color: 'white' }}>Contact</Link>
+    <nav style={{ display: 'flex', justifyContent: 'space-around', padding: '10px', backgroundColor: '#eee' }}>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/contact">Contact</Link>
     </nav>
   );
 }
+
 export default Navbar;
