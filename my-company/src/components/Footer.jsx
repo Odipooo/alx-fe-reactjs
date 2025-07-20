a6fd4bbfb7a7271223defb6bf38fb2bc607bce9a
Nick Odipo <nickodips@gmail.com>
2025-07-20 18:31:47 +0300

    Add my-company React website with routing and contact form

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', backgroundColor: '#eee', textAlign: 'center' }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+export default Footer;
