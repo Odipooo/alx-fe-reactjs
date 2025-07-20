0f9198cc11ce09fdf9359aa5bc34f46a95f8c0a7
Nick Odipo <nickodips@gmail.com>
2025-07-20 17:51:48 +0300

    Enhanced components in alx-react-app-new with inline CSS

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '15px', backgroundColor: '#f0f8ff', fontSize: '18px' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
