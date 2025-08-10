0ebcab6af6bc392aea3979971456821be7ff0067
Nick Odipo <nickodips@gmail.com>
2025-08-10 09:52:44 +0300

    Updated user profile text to look good on phones and bigger screens

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,14 +1,16 @@
 function UserProfile() {
   return (
-         <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
         className="rounded-full w-36 h-36 mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4 font-semibold">John Doe</h1>
-      <p className="text-gray-600 text-base">
-        Footballer at Barcelona Loves to play football and make fashion.
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 font-semibold">
+        Nick Odipo
+      </h1>
+      <p className="text-gray-600 text-sm">
+        Footballer at Barcelona Loves to play football and make fashion
       </p>
     </div>
   );
