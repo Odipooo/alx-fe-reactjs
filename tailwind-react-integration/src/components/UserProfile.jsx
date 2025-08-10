4f51711d06ca7f65cddb656ea241d77ab102a19c
Nick Odipo <nickodips@gmail.com>
2025-08-10 09:32:33 +0300

    Added a user profile component with Tailwind CSS

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,17 @@
+function UserProfile() {
+  return (
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="rounded-full w-36 h-36 mx-auto"
+      />
+      <h1 className="text-xl text-blue-800 my-4 font-semibold">Nick Odipo</h1>
+      <p className="text-gray-600 text-base">
+        Footballer at Barcelona Loves to play football and make fashion.
+      </p>
+    </div>
+  );
+}
+
+export default UserProfile;
